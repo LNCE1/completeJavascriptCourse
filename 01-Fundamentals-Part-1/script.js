677cb4d0d418052d152c02a19fdc36854d0735d1
Nicolas Clerici Erhardt <nicolasclericierhardt@gmail.com>
2024-08-13 22:17:32 +1000

    Added notes and exercises about Operator Precedence

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -100,13 +100,16 @@ console.log(now - 1991 > now - 2018);
 
 */
 
+/*
 ////////////////////////////////////////////////////////////////////////
 // Operator Precedence
+//We can check the following table of the operator precedence
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
 
-console.log(now - 1991 > now - 2018);
+console.log(now - 1991 > now - 2018); //Precedence favors subtraction first, then "greater than >  "
 
 let x, y;
 x = y = 25 - 10 - 5; // x = y = 10, x = 10
@@ -114,27 +117,26 @@ console.log(x, y);
 
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
+*/
 
 ////////////////////////////////////////////////////////////////////////
 //  Coding Challenge #1
 
-/*
-Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+// Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
 
-1. Store Mark's and John's mass and height in variables
-2. Calculate both their BMIs using the formula (you can even implement both versions)
-3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+// 1. Store Mark's and John's mass and height in variables
+// 2. Calculate both their BMIs using the formula (you can even implement both versions)
+// 3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
 
-TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
-TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+// TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+// TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
 
-GOOD LUCK 😀
-*/
+// GOOD LUCK 😀
 
-// const massMark = 78;
-// const heightMark = 1.69;
-// const massJohn = 92;
-// const heightJohn = 1.95;
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
 
 /*
 const massMark = 95;
