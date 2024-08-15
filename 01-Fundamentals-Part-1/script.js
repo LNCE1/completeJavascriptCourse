1e9cf0a3a8990e77b0b04029b59d91b599895017
Nicolas Clerici Erhardt <nicolasclericierhardt@gmail.com>
2024-08-15 22:21:43 +1000

    Added exercises and notes regarding truthy and falsy values

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -261,6 +261,7 @@ if (BMIMark > BMIJohn) {
 }
 */
 
+/*
 ////////////////////////////////////////////////////////////////////////
 // Type Conversion and Coercion
 
@@ -282,58 +283,62 @@ console.log("23" / "2");
 let n = "1" + 1; // '11'
 n = n - 1;
 console.log(n);
-/*
- */
 
-/*
+*/
+
+/* 
 ////////////////////////////////////////////////////////////////////////
 // Truthy and Falsy Values
-
+//In JavaScript, a truthy value is a value that is considered true when encountered in a Boolean context. All values are truthy unless they are defined as falsy. That is, all values are truthy except false, 0, -0, 0n, "", null, undefined, NaN, and document.all.
 // 5 falsy values: 0, '', undefined, null, NaN
 console.log(Boolean(0));
 console.log(Boolean(undefined));
-console.log(Boolean('Jonas'));
+console.log(Boolean("Jonas"));
 console.log(Boolean({}));
-console.log(Boolean(''));
+console.log(Boolean(""));
 
 const money = 100;
 if (money) {
   console.log("Don't spend it all ;)");
 } else {
-  console.log('You should get a job!');
+  console.log("You should get a job!");
 }
 
 let height = 0;
 if (height) {
-  console.log('YAY! Height is defined');
+  console.log("YAY! Height is defined");
 } else {
-  console.log('Height is UNDEFINED');
+  console.log("Height is UNDEFINED");
 }
+*/
 
-////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 // Equality Operators: == vs. ===
-const age = '18';
-if (age === 18) console.log('You just became an adult :D (strict)');
+const age = "18";
+if (age === 18) console.log("You just became an adult :D (strict)");
 
-if (age == 18) console.log('You just became an adult :D (loose)');
+if (age == 18) console.log("You just became an adult :D (loose)");
 
 const favourite = Number(prompt("What's your favourite number?"));
 console.log(favourite);
 console.log(typeof favourite);
 
-if (favourite === 23) { // 22 === 23 -> FALSE
-  console.log('Cool! 23 is an amzaing number!')
+if (favourite === 23) {
+  // 22 === 23 -> FALSE
+  console.log("Cool! 23 is an amzaing number!");
 } else if (favourite === 7) {
-  console.log('7 is also a cool number')
+  console.log("7 is also a cool number");
 } else if (favourite === 9) {
-  console.log('9 is also a cool number')
+  console.log("9 is also a cool number");
 } else {
-  console.log('Number is not 23 or 7 or 9')
+  console.log("Number is not 23 or 7 or 9");
 }
 
-if (favourite !== 23) console.log('Why not 23?');
+if (favourite !== 23) console.log("Why not 23?");
+/* */
 
-////////////////////////////////////
+/*
+////////////////////////////////////////////////////////////////////////
 // Logical Operators
 const hasDriversLicense = true; // A
 const hasGoodVision = true; // B
@@ -357,11 +362,11 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
   console.log('Someone else should drive...');
 }
 */
-
+/*
 ////////////////////////////////////
 // Coding Challenge #3
 
-/*
+
 There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
 
 1. Calculate the average score for each team, using the test data below
