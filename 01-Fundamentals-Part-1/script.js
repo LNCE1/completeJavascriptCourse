b3639e6a222b11ca366cbcff24db60f7dcffc570
Nicolas Clerici Erhardt <nicolasclericierhardt@gmail.com>
2024-08-18 23:22:14 +1000

    commenting code

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -518,6 +518,7 @@ HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
 
 GOOD LUCK 😀
 */
+/*
 //MY SOLUTION
 const bill = 430;
 const tip = bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
@@ -530,3 +531,4 @@ console.log(
 // console.log(
 //   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
 // );
+*/
