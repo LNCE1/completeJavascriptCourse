9aadd41efeb7f656c7f679182ace1623983f3526
Nicolas Clerici Erhardt <nicolasclericierhardt@gmail.com>
2024-08-21 22:34:57 +1000

    Added exercises and notes about arrays

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -196,45 +196,65 @@ checkWinner(scoreDolphins, scoreKoalas);
 /*
 ///////////////////////////////////////
 // Introduction to Arrays
-const friend1 = 'Michael';
-const friend2 = 'Steven';
-const friend3 = 'Peter';
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
 
-const friends = ['Michael', 'Steven', 'Peter'];
-console.log(friends);
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends); //[ 'Michael', 'Steven', 'Peter' ]
 
 const y = new Array(1991, 1984, 2008, 2020);
 
-console.log(friends[0]);
-console.log(friends[2]);
+console.log(friends[0]); //Michael
+console.log(friends[2]); //Peter
+console.log(friends.length); //3
+console.log(friends[friends.length - 1]); //Peter
 
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
+friends[2] = "Jay"; //Swap "Peter" for "Jay"
+console.log(friends); //[ 'Michael', 'Steven', 'Jay' ]
 
-friends[2] = 'Jay';
-console.log(friends);
-// friends = ['Bob', 'Alice']
-
-const firstName = 'Jonas';
-const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+const firstName = "Jonas";
+const jonas = [firstName, "Schmedtmann", 2037 - 1991, "teacher", friends];
 console.log(jonas);
-console.log(jonas.length);
 
-// Exercise
-const calcAge = function (birthYeah) {
-  return 2037 - birthYeah;
-}
+[
+  'Jonas',
+  'Schmedtmann',
+  46,
+  'teacher',
+  [ 'Michael', 'Steven', 'Jay' ]
+] 
+
+console.log(jonas.length); //5
+
+//JONAS SOLUTION with current subject
+// Exercise: calculate ages from the birthyears of the array
+// const years = [1990, 1967, 2002, 2010, 2018];
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// const ages = [
+//   calcAge(years[0]),
+//   calcAge(years[1]),
+//   calcAge(years[years.length - 1]),
+// ];
+// console.log(ages);
+
+//MY SOLUTION with for loop
+const calcAge = function (birthYear) {
+  return 2024 - birthYear;
+};
+
 const years = [1990, 1967, 2002, 2010, 2018];
-
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[years.length - 1]);
-console.log(age1, age2, age3);
-
-const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
-console.log(ages);
-
-
+let ages = [];
+for (let i = 0; i < years.length; i++) {
+  ages.push(calcAge(years[i]));
+}
+console.log(ages); //[ 34, 57, 22, 14, 6 ]
+*/
+/*
 ///////////////////////////////////////
 // Basic Array Operations (Methods)
 const friends = ['Michael', 'Steven', 'Peter'];
